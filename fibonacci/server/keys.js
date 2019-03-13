a97a71a9b1cb455e29082db872e1c3641ddce4eb
Oleg <oyapparov@gmail.com>
2019-03-13 21:19:42 +0100

    Remove fibonacci project

--- fibonacci/server/keys.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  pgHost: process.env.PGHOST,
-  pgDatabase: process.env.PGDATABASE,
-  pgPassword: process.env.PGPASSWORD,
-  pgPort: process.env.PGPORT,
-  pgUser: process.env.PGUSER,
-  redisHost: process.env.REDIS_HOST,
-  redisPort: process.env.REDIS_PORT
-};
